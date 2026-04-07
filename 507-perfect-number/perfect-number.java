5402e10decec33d0cdf58df23aac6a373021345a
Rajni00000 <rajnirajniray@gmail.com>
2026-04-07 14:30:40 +0530

    Time: 3 ms (69.01%) | Memory: 42.4 MB (16.89%) - LeetSync

---
+++ 507-perfect-number/perfect-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if (num <= 1) {
+            return false;
+        }
+
+        int sum = 1; // 1 is always a divisor
+
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                if (i != num / i) {
+                    sum += i + num / i;
+                } else {
+                    sum += i;
+                }
+            }
+        }
+
+        return sum == num;
+    }
+}
